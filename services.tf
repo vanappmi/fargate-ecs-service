50ff3ba42b4ee26fcee456dc32e3282e2ee6efb7
Michael van Appeldorn <michael.vanappeldorn@cognotekt.com>
2021-02-26 15:09:33 +0100

    fix repo url

--- services.tf
+++ services.tf
@@ -4,13 +4,12 @@ variable "name" {
 }
 
 module "bar" {
-  source = "git::git@ssh.dev.azure.com:v3/consilica/terraform-modules/ecs-service"
-  #source          = "../terraform-modules/ecs-service"
+  source          = "../terraform-modules/ecs-service"
   name_prefix     = var.name
-  app             = "testapp"
-  container_image = "085161355646.dkr.ecr.eu-central-1.amazonaws.com/${var.name}"
+  app             = "app"
+  container_image = "###REPLACE WITH ECR REPO URL###"
   container_name  = var.name
-  cluster_name    = "fargate"
+  cluster_name    = "foo"
   # container doesn't have HTTPS endpoint
   lb_https_ports = {}
 }
